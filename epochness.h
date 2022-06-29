d4cfb80cf7ff21caaa0eccc232f19b7df390d859
Sean Jackson <105176571+seanjackson6325@users.noreply.github.com>
2022-06-29 01:11:25 -0400

    Update epochness.h

--- epochness.h
+++ epochness.h
@@ -13,36 +13,43 @@
 
 #ifdef _WIN32
 
-	// for time functions:
-	#include "windows.h"
+// for time functions:
+#include "windows.h"
 
-  	// type for keeping track of ticks
-	typedef unsigned long long epoch_int;
+// type for keeping track of ticks
+typedef unsigned long long epoch_int;
 
-	// struct for epoch:
-	typedef struct
-	{
-		epoch_int last, now;
-	} Epoch_t;
+// struct for epoch:
+typedef struct
+{
+	epoch_int last, now;
+} Epoch_t;
 
 /////////////////////////////////////////////////////////////////
 
 #else
 
-	#include <time.h>
-	#include <sys/time.h>
+#include <time.h>
+#include <sys/time.h>
 
-	typedef struct timespec timespec;
+// timespec holds seconds and nanoseconds
+typedef struct timespec timespec;
 
-	typedef struct
-	{
-		timespec last, now;
-	} Epoch_t;
+// struct for epoch:
+typedef struct
+{
+	timespec last, now;
+} Epoch_t;
 
 /////////////////////////////////////////////////////////////////
 
 #endif
 
+// initialize epochness
 bool Epoch_Init();
+// start an epoch timer
 void Epoch_Start(Epoch_t* e);
+// return time (seconds) since start
 double Epoch_QueryChange(Epoch_t* e);
+
+/////////////////////////////////////////////////////////////////
