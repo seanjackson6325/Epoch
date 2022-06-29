3c32ce2857cbb2b99878a86ade07e22f66e57038
Sean Jackson <105176571+seanjackson6325@users.noreply.github.com>
2022-06-29 00:15:22 -0400

    Update epochness.c

--- epochness.c
+++ epochness.c
@@ -11,11 +11,11 @@
 
 /////////////////////////////////////////////////////////////////
 
-// the tick frequency constant
-// (conversion from ticks to seconds)
-epoch_int epoch_query_frequency = 0;
+#ifdef _WIN32
 
-#ifdef WIN32
+	// the tick frequency constant
+	// (conversion from ticks to seconds)
+	epoch_int epoch_query_frequency = 0;
 	
 	// prepare the epochness library for use
 	bool Epoch_Init()
@@ -31,60 +31,12 @@ epoch_int epoch_query_frequency = 0;
 	
 	// get the change in ticks from the last time this function
 	// was called (or when the epoch was started if called for the first time)
-	epoch_int Epoch_QueryChange(Epoch_t* e)
+	double Epoch_QueryChange(Epoch_t* e)
 	{
 		QueryPerformanceCounter(&e->now);
 		epoch_int change = e->now - e->last;
 		e->last = e->now;
-		return change;
-	}
-
-	// return the tick frequency constant
-	epoch_int Epoch_TicksPerSecond()
-	{
-		return epoch_query_frequency;
-	}
-
-	// convert from ticks to seconds
-	double Epoch_TicksToSeconds(epoch_int ticks)
-	{
-		return (double)ticks / epoch_query_frequency;
-	}
-
-	void Epoch_GameLoop(bool* run, const int UPS, void(update)(void), void(render)(void), char* report_buff)
-	{
-		Epoch_t clock;
-		Epoch_Start(&clock);
-
-		double d_ticks = 0.0;
-		double TPS = (double)Epoch_TicksPerSecond();
-		double TPU = TPS / UPS;
-
-		int ups = 0;
-		int fps = 0;
-		double report = 0.0;
-
-		while (*run)
-		{
-			epoch_int delta = Epoch_QueryChange(&clock);
-			d_ticks += (double) delta;
-			report += (double) delta;
-			if (d_ticks >= TPU)
-			{
-				d_ticks -= TPU;
-				update();
-				ups++;
-			}
-			render();
-			fps++;
-
-			if (report >= TPS)
-			{
-				report -= TPS;
-				printf("UPS:%d\tFPS:%d\n", ups, fps);
-				fps = ups = 0.0;
-			}
-		}
+		return (double)change / epoch_query_frequency;
 	}
 
 #else
