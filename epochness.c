eddc5cf0cd4f30bea1f270c487bee26ca301d37e
Sean Jackson <105176571+seanjackson6325@users.noreply.github.com>
2022-06-29 01:12:11 -0400

    Update epochness.c

--- epochness.c
+++ epochness.c
@@ -13,57 +13,69 @@
 
 #ifdef _WIN32
 
-	// the tick frequency constant
-	// (conversion from ticks to seconds)
-	epoch_int epoch_query_frequency = 0;
+// the tick frequency constant
+// (conversion from ticks to seconds)
+epoch_int epoch_query_frequency = 0;
 
-	// prepare the epochness library for use
-	bool Epoch_Init()
-	{
-		QueryPerformanceFrequency(&epoch_query_frequency);
-	}
+// prepare the epochness library for use
+bool Epoch_Init()
+{
+	QueryPerformanceFrequency(&epoch_query_frequency);
+}
 
-	// start the epoch
-	void Epoch_Start(Epoch_t* e)
-	{
-		QueryPerformanceCounter(&e->last);
-	}
+// start the epoch
+void Epoch_Start(Epoch_t* e)
+{
+	QueryPerformanceCounter(&e->last);
+}
 
-	// get the change in ticks from the last time this function
-	// was called (or when the epoch was started if called for the first time)
-	double Epoch_QueryChange(Epoch_t* e)
-	{
-		QueryPerformanceCounter(&e->now);
-		epoch_int change = e->now - e->last;
-		return (double)change / epoch_query_frequency;
-	}
+/*
+* Get the change in time (seconds)
+* since the timer was started
+*/
+double Epoch_QueryChange(Epoch_t* e)
+{
+	QueryPerformanceCounter(&e->now);
+	epoch_int change = e->now - e->last;
+	return (double)change / epoch_query_frequency;
+}
+
+/////////////////////////////////////////////////////////////////
 
 #else
 
-	bool Epoch_Init() {}
+// prepare the epochness library for use
+bool Epoch_Init() {}
 
-	void Epoch_Start(Epoch_t* e)
+// start the epoch
+void Epoch_Start(Epoch_t* e)
+{
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &e->last);
+}
+
+/*
+* Get the change in time (seconds)
+* since the timer was started
+*/
+double Epoch_QueryChange(Epoch_t* e)
+{
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &e->now);
+	timespec diff;
+
+	if (e->now.tv_nsec - e->last.tv_nsec < 0)
 	{
-		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &e->last);
+		diff.tv_sec = e->now.tv_sec - e->last.tv_sec - 1;
+		diff.tv_nsec = 1000000000 + e->now.tv_nsec - e->last.tv_nsec;
+	}
+	else
+	{
+		diff.tv_sec = e->now.tv_sec - e->last.tv_sec;
+		diff.tv_nsec = e->now.tv_nsec - e->last.tv_nsec;
 	}
 
-	double Epoch_QueryChange(Epoch_t* e)
-	{
-		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &e->now);
-		timespec diff;
+	return diff.tv_sec + diff.tv_nsec / 1000000000.0;
+}
 
-		if(e->now.tv_nsec - e->last.tv_nsec < 0)
-		{
-			diff.tv_sec = e->now.tv_sec - e->last.tv_sec-1;
-			diff.tv_nsec = 1000000000 + e->now.tv_nsec - e->last.tv_nsec;
-		}
-		else
-		{
-			diff.tv_sec = e->now.tv_sec - e->last.tv_sec;
-			diff.tv_nsec = e->now.tv_nsec - e->last.tv_nsec;
-		}
-
-		return diff.tv_sec + diff.tv_nsec / 1000000000.0;
-	}
+/////////////////////////////////////////////////////////////////
 
 #endif
